73067911705062ad76063efa39b010a3a6806f18
AgustinPagotto <agustinpagotto@gmail.com>
2024-08-17 15:26:46 -0300

    Vial port/ finished qmk firmware

---
+++ Firmware/keymaps/vial/config.h
@@ -0,0 +1,5 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#pragma once
+#define VIAL_KEYBOARD_UID {0xD4, 0xED, 0xC7, 0x63, 0xF9, 0x26, 0xE1, 0x08}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 15 }
